bf1cbc7102169f636b05f830c6bbe6973e904fc4
Sam Murray <samvmurray@gmail.com>
2019-03-04 09:02:40 -0600

    Attempt 1 at JSON functionality

--- loadPage.js
+++ loadPage.js
@@ -1,6 +1,7 @@
 "use strict";
 window.onload = function ()
 {
+    console.log("JS Functionality");
 	var req = new XMLHttpRequest();
 	req.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
