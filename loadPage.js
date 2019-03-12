7303267eca566cdb030f667ba6a20c09567a10f0
Sam Murray <samvmurray@gmail.com>
2019-03-12 17:02:57 -0500

    Beta

--- loadPage.js
+++ loadPage.js
@@ -1,38 +1,40 @@
 "use strict";
 window.onload = function ()
 {
-    console.log("JS Functionality");
 	var req = new XMLHttpRequest();
 	req.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
-	    console.log(JSON.parse(this.responseText));
 	    var jsonData = JSON.parse(this.responseText);	    
 	    var req2 = new XMLHttpRequest();
 	    req2.onreadystatechange = function() 
 	    {
 	        if (this.readyState == 4 && this.status == 200) 
 	        {
-	           /* if (typeof this.responseText == "string")
-	            {*/
-	                var xmlObj = (new window.DOMParser()).parseFromString(this.responseText, "text/xml");
-	                var langObj;
-	                if (xmlObj.activeElement != undefined && xmlObj.activeElement != null){langObj = xmlObj.activeElement.childNodes[1].childNodes;}
-	                else {langObj = xmlObj.getElementsByTagName("langs")[0].getElementsByTagName("Item");}
-                    var ddm = createElement("div", "", "ddDiv");
-                    ddm.appendChild(createElement("button", "Translate", "ddButton", "translatable"));
-                    var ddc = createElement("div", "", "ddContainer");
-                    for (var i in langObj)
+                var xmlObj = (new window.DOMParser()).parseFromString(this.responseText, "text/xml");
+                var langObj;
+                if (xmlObj.activeElement != undefined && xmlObj.activeElement != null) {
+                    //Firefox
+                    langObj = xmlObj.activeElement.childNodes[1].childNodes;
+                }
+                else 
+                {
+                    //Chrome
+                    langObj = xmlObj.getElementsByTagName("langs")[0].getElementsByTagName("Item");
+                }
+                var ddm = createElement("div", "", "ddDiv");
+                ddm.appendChild(createElement("button", "Translate", "ddButton", "translatable"));
+                var ddc = createElement("div", "", "ddContainer");
+                for (var i in langObj)
+                {
+                    if (langObj[i].attributes != undefined)
                     {
-                        if (langObj[i].attributes != undefined)
-                        {
-                            var curr = createElement("div", langObj[i].getAttribute("value"), langObj[i].getAttribute("key"));
-                            ddc.appendChild(curr);
-                            curr.onclick = translate;
-                        }
+                        var curr = createElement("div", langObj[i].getAttribute("value"), langObj[i].getAttribute("key"));
+                        ddc.appendChild(curr);
+                        curr.onclick = translate;
                     }
-                    ddm.appendChild(ddc);
-                    holdNav.appendChild(ddm);
-	            //}
+                }
+                ddm.appendChild(ddc);
+                holdNav.appendChild(ddm);
             }
             else if (this.readyState == 4) {console.log(this.status);};
         };
@@ -64,6 +66,14 @@ window.onload = function ()
 	req.send();
 }
 
+document.onkeydown = function (e) {
+    if (document.getElementById("imgDiv") != undefined)
+    {
+        if (e.keyCode == '37' ) {nextPic(document.getElementById("nextPic").data);}
+        else if (e.keyCode == '39') {prevPic(document.getElementById("nextPic").data);}
+    }
+} 
+
 function loadHome(json)
 {
     var pDiv = createElement("div", "", "pDiv");   
@@ -74,7 +84,7 @@ function loadHome(json)
     var newsDiv = createElementWithStyleProps("div", "", "newsDiv", ["marginTop"], ["2rem"]);
 	newsDiv.appendChild(createElement("h3", "Recent News:", "newsHead", "translatable"));
     var newsList = newsDiv.appendChild(createElement("ul", "", "newsList"));
-    for (var i in json.home.news) {newsList.appendChild(createElement("li", json.home.news[i].item, "newsItem" + i, "translatable")); }
+    for (var i in json.home.news) {newsList.appendChild(createElement("li", json.home.news[i].item, "newsItem" + i, "translatable"));}
     document.getElementById("content").insertBefore(newsDiv, document.getElementById("footer"));
 }
 
@@ -85,14 +95,8 @@ function loadProjects(json)
 	for (var i in json.projects)
 	{
     	var currDiv = createElement("div", "", "projDiv" + i);
-    	var currImg = createImageWithProps("projImg" + i, json.projects[i].images[1].item, json.projects[i].title,["width", "float", "marginRight", "clear", "verticalAlign"], ["3em", "left", "1vw", "left", "middle"]);
-    	currImg.onclick = lb;
-    	currImg.className = "lbImg";
-    	currImg.dataLightbox = "testSet" + i;
-    	
-    	/*LightBox Shit goes here*/
-    	
-    	
+    	var currImg = createImageWithProps("projImg" + i, json.projects[i].images[0].item, json.projects[i].title,["width", "float", "marginRight", "clear", "verticalAlign"], ["3em", "left", "1vw", "left", "middle"], "lbImg");
+    	currImg.onclick = function(){lb(this, json);};
 	    currDiv.appendChild(currImg);
 	    currDiv.appendChild(createElementWithStyleProps("div", json.projects[i].title, "projTit" + i, ["fontWeight"], ["bold"], "translatable"));
 	    currDiv.appendChild(createElement("div", json.projects[i].desc, "projDesc" + i, "translatable"));
@@ -102,9 +106,45 @@ function loadProjects(json)
     document.getElementById("content").insertBefore(projDiv, document.getElementById("footer"));
 }
 
-function lb()
+function lb(obj, json)
 {
-    window.alert("test");
+    var idx = obj.id.substring(obj.id.length-1);
+    var imgDiv;
+    if (document.getElementById("imgDiv") == undefined)
+    {
+        imgDiv = createElementWithStyleProps("div", "", "imgDiv", ["backgroundColor", "position", "top", "left", "height", "width"], ["black", "fixed", "0", "0", "100%", "100%"]);
+        var projImg = createImageWithProps("projImg", json.projects[idx].images[1].item, json.projects[idx].title,["position", "marginTop", "verticalAlign", "zIndex", "height", "display", "marginLeft", "marginRight"], ["relative", "10vh", "middle", ".9", "80%", "block", "auto", "auto"]);
+        imgDiv.appendChild(projImg);
+        var close = createElementWithStyleProps("button", "X", "close", ["position", "top", "right", "zIndex"], ["fixed", "11vh", "5vw", "1"])
+        close.onclick = function(){makeHidden(imgDiv);};
+        close.data = 1;
+        
+        var right = createElementWithStyleProps("button",">", "nextPic", ["position", "top", "right", "zIndex"], ["fixed", "50vh", "5vw", "1"]);
+        right.onclick = function(){nextPic(json.projects[idx], idx);};
+        right.data = json.projects[idx];
+
+        var left = createElementWithStyleProps("button","<", "prevPic", ["position", "top", "left", "zIndex"], ["fixed", "50vh", "5vw", "1"]);
+        left.onclick = function(){prevPic(json.projects[idx], idx);};
+        
+        imgDiv.appendChild(createElementWithStyleProps("div", json.projects[idx].title + "<br />" + json.projects[idx].desc + "<br />" + "1/" + (json.projects[idx].images.length -1), "desc", ["margin", "width", "textAlign", "position", "color", "zIndex", "bottom", "paddingLeft", "backgroundColor", "paddingTop", "paddingRight", "fontSize"], ["0 auto", "36%", "center", "absolute", "white", "1", "1vh", "32%", "rgba(0,0,0,.6)", "1%", "32%", "120%"]));
+        
+        imgDiv.appendChild(close);
+        imgDiv.appendChild(right);
+        imgDiv.appendChild(left);
+        document.getElementById("content").appendChild(imgDiv);
+    }
+    else
+    {
+        imgDiv = document.getElementById("imgDiv"); 
+        document.getElementById("projImg").src = json.projects[idx].images[1].item;
+        imgDiv.style.visibility = "visible";
+        document.getElementById("desc").innerHTML = json.projects[idx].title + "<br />" + json.projects[idx].desc + "<br />" + "1/" + (json.projects[idx].images.length -1);
+        document.getElementById("nextPic").onclick = function(){nextPic(json.projects[idx]);};
+        document.getElementById("prevPic").onclick = function(){prevPic(json.projects[idx]);};
+        document.getElementById("close").onclick = function(){makeHidden(imgDiv);};
+        document.getElementById("nextPic").data = json.projects[idx];
+        document.getElementById("close").data = 1;
+    }
 }
 
 
@@ -153,52 +193,6 @@ function getJSON(addr, id)
 	return p;
 }
 
-function translate()
-{
-    var els = document.getElementsByClassName("translatable");
-    for (var i in els)
-    {
-        if(els[i].id != undefined)
-        {
-            var addr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190311T164242Z.a3ee938d0c540e06.58e36ab370c756038a42d6b4148e9e68101881f7&text=" + encodeURI(els[i].innerHTML) + "&lang=en-" + this.id; 
-            getJSON(addr, els[i].id).then(results => {document.getElementById(results[1]).innerHTML = results[0].text[0];});
-        }
-    }
-    console.log("endTrans");
-}
-
-function createImageWithProps(id, src, alt, props, vals)
-{
-    var result = createElement("img", "", id);
-    result.src = src;
-    result.alt = alt;
-    for (var i in props) {result.style[props[i]] = vals[i];}
-    return result;
-}
-
-function createElementWithStyleProps(type, content, id, props, values, className)
-{
-    var result = createElement(type, content, id, className);
-    for (var i in props) {result.style[props[i]] = values[i];}
-    return result;
-}
-
-function createLink(content, id, addr, blankTF, className)
-{
-    var result = createElement("a", content, id, className);
-    result.href = addr;
-    if (blankTF) {result.target = "blank";}
-    return result;
-}
-
-function createElement(type, content, id, className)
-{
-	var element1 = document.createElement(type);
-	element1.innerHTML = content;
-	element1.id = id;
-	element1.className = className;
-	return element1;
-}
 
 function loadResume(json)
 {
@@ -280,15 +274,98 @@ function loadResume(json)
 	    {
 	        var currEl = createElement("li", json.resume.leadership[i].value, "lead" + i, "translatable");
 	        var currList = currEl.appendChild(createElement("ul", "", json.resume.leadership[i].data + "list", "translatable"));
-	        for (var j in json.resume.leadership[i].data) {currList.appendChild(createElement("li", json.resume.leadership[i].data[j].info, "lead" + i + "_" + j, "translatable"));}
+	        for (var j in json.resume.leadership[i].data) 
+	        {
+	            currList.appendChild(createElement("li", json.resume.leadership[i].data[j].info, "lead" + i + "_" + j, "translatable"));
+            }
 	        leadList.appendChild(currEl);
 	    }
-	    else {leadList.appendChild(createElement("li", json.resume.leadership[i].value, "lead" + i, "translatable"));}
+	    else 
+	    {
+	        leadList.appendChild(createElement("li", json.resume.leadership[i].value, "lead" + i, "translatable"));
+	    }
     }
     document.getElementById("content").insertBefore(leadDiv, document.getElementById("footer"));
     var skillDiv = createElementWithStyleProps("div", "", "skillDiv", ["marginTop"], ["2em"]);
     skillDiv.appendChild(createElement("h3", "Skills", "skillHead", "translatable"));
     var skillList = skillDiv.appendChild(createElement("ul", "", "skillList"));
-    for (i in json.resume.skills) {skillList.appendChild(createElement("li", json.resume.skills[i].value, "skill" + i, "translatable"));}
+    for (i in json.resume.skills) 
+    {
+        skillList.appendChild(createElement("li", json.resume.skills[i].value, "skill" + i, "translatable"));
+    }
     document.getElementById("content").insertBefore(skillDiv, document.getElementById("footer")); 
+}
+
+function appendChildren(parent, children)
+{
+    for (i in children)
+    {
+        parent.appendChild(children[i]);
+    }
+}
+
+function createElement(type, content, id, className)
+{
+	var element1 = document.createElement(type);
+	element1.innerHTML = content;
+	element1.id = id;
+	element1.className = className;
+	return element1;
+}
+
+function createElementWithStyleProps(type, content, id, props, values, className)
+{
+    var result = createElement(type, content, id, className);
+    for (var i in props) {result.style[props[i]] = values[i];}
+    return result;
+}
+
+function createImageWithProps(id, src, alt, props, vals, className)
+{
+    var result = createElement("img", "", id);
+    result.src = src;
+    result.alt = alt;
+    result.className = className;
+    for (var i in props) {result.style[props[i]] = vals[i];}
+    return result;
+}
+
+function createLink(content, id, addr, blankTF, className)
+{
+    var result = createElement("a", content, id, className);
+    result.href = addr;
+    if (blankTF) {result.target = "blank";}
+    return result;
+}
+
+function makeHidden (el) {el.style.visibility = "hidden";}
+
+function nextPic(info)
+{
+    var imgIdx = Math.max(1,(document.getElementById("close").data + 1) % info.images.length);
+    document.getElementById("projImg").src = info.images[imgIdx].item;
+    document.getElementById("close").data = imgIdx;
+    document.getElementById("desc").innerHTML = info.title + "<br />" + info.desc + "<br />" + imgIdx + "/" + (info.images.length -1);
+}
+
+function prevPic(info)
+{
+    var imgIdx = document.getElementById("close").data - 1;
+    if (imgIdx == 0) { imgIdx = info.images.length-1;}
+    document.getElementById("projImg").src = info.images[imgIdx].item;
+    document.getElementById("close").data = imgIdx;
+    document.getElementById("desc").innerHTML = info.title + "<br />" + info.desc + "<br />" + imgIdx + "/" + (info.images.length -1);
+}   
+
+function translate()
+{
+    var els = document.getElementsByClassName("translatable");
+    for (var i in els)
+    {
+        if(els[i].id != undefined)
+        {
+            var addr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190311T164242Z.a3ee938d0c540e06.58e36ab370c756038a42d6b4148e9e68101881f7&text=" + encodeURI(els[i].innerHTML) + "&lang=en-" + this.id; 
+            getJSON(addr, els[i].id).then(results => {document.getElementById(results[1]).innerHTML = results[0].text[0];});
+        }
+    }
 }
